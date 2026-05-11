1f09d221dff34afa26971f6d14bfb6f1d2e59630
alione10-lm <aliouaneoussama89@gmail.com>
2026-05-11 12:20:54 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="text-xl text-indigo-500">
+            Smart Invoice Payment Tracking frontend
+        </div>
+    );
+};
+
+export default App;
