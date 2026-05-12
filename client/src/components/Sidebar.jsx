37e97fe5dd921d1300ac06d8b6736bc69943ade9
alione10-lm <aliouaneoussama89@gmail.com>
2026-05-12 15:40:23 +0100

    change logo sytling

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,11 @@
 import { NavLink } from "react-router-dom";
-import { Home, Newspaper, Users2 } from "lucide-react";
+import {
+    Home,
+    MousePointer2,
+    Newspaper,
+    TextCursorIcon,
+    Users2,
+} from "lucide-react";
 
 const NAVLINKS = {
     dashboard: {
@@ -20,12 +26,21 @@ const NAVLINKS = {
 const Sidebar = () => {
     return (
         <aside className="w-64 min-h-screen border-r border-secondary bg-background  p-4">
-            <h1 className="my-10">smart invoices</h1>
+            <div className="mb-16 border-b border-secondary pb-4 mt-10 flex items-center font-semibold text-xl gap-2">
+                <div className="p-2 bg-secondary border border-primary/30 rounded flex items-center justify-center">
+                    <MousePointer2 size={20} className="stroke-primary " />
+                </div>
+                <h1 className="">
+                    <span>Invoice</span>
+                    <span className="text-primary">Pilot</span>
+                </h1>
+            </div>
+
             {Object.entries(NAVLINKS).map(([key, link]) => (
                 <NavLink
                     key={key}
                     to={`/${key === "dashboard" ? "" : key}`}
-                    className="  flex items-center p-3   gap-4 rounded hover:bg-background/30 text-secondary-foreground hover:text-primary mb-1"
+                    className="  flex items-center p-3   gap-4 rounded text-secondary-foreground hover:text-primary hover:bg-primary/10 mb-1"
                 >
                     {link.icon}
                     {link.name}
